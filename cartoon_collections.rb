92a13dc74be26b9fa12ed866b4d7bec3cc38242d
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-05-27 23:13:52 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(array)
   new_arr = array.each_with_index do |index, name|
     puts "{index + 1} {name}"
   end  
-  new_arr.split(\n)
+  new_arr.split("\n")
 end
 
 def summon_captain_planet# code an argument here
